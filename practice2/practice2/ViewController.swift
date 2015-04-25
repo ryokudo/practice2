34fab5333a5c9963b454b9c32995c6d1d0cd7034
ryokudo <kr810.com@gmail.com>
2015-04-25 14:22:38 +0900

    20150425 テストコミット

---
+++ practice2/practice2/ViewController.swift
@@ -0,0 +1,148 @@
+//
+//  ViewController.swift
+//  git_Practice
+//
+//  Created by Shinya Sugawara on 4/18/15.
+//  Copyright (c) 2015 Shinya Sugawara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController , UITextFieldDelegate{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Labelを作成.
+        let myLabel: UILabel = UILabel(frame: CGRectMake(0,0,200,50))
+        
+        // 背景をオレンジ色にする.
+        myLabel.backgroundColor = UIColor.orangeColor()
+        
+        // 枠を丸くする.
+        myLabel.layer.masksToBounds = true
+        
+        // コーナーの半径.
+        myLabel.layer.cornerRadius = 20.0
+        
+        // Labelに文字を代入.
+        myLabel.text = "Hello Swift!!"
+        
+        // 文字の色を白にする.
+        myLabel.textColor = UIColor.whiteColor()
+        
+        // 文字の影の色をグレーにする.
+        myLabel.shadowColor = UIColor.grayColor()
+        
+        // Textを中央寄せにする.
+        myLabel.textAlignment = NSTextAlignment.Center
+        
+        // 配置する座標を設定する.
+        myLabel.layer.position = CGPoint(x: self.view.bounds.width/2,y: 200)
+        
+        // Viewの背景色を青にする.
+        self.view.backgroundColor = UIColor.cyanColor()
+        
+        // ViewにLabelを追加.
+        self.view.addSubview(myLabel)
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let button = UIButton()
+        
+        //表示されるテキスト
+        button.setTitle("Tap Me!", forState: .Normal)
+        
+        //テキストの色
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        
+        //タップした状態のテキスト
+        button.setTitle("Tapped!", forState: .Highlighted)
+        
+        //タップした状態の色
+        button.setTitleColor(UIColor.redColor(), forState: .Highlighted)
+        
+        //サイズ
+        button.frame = CGRectMake(0, 0, 300, 50)
+        
+        //配置場所
+        button.layer.position = CGPoint(x: self.view.frame.width/2, y:100)
+        
+        //背景色
+        button.backgroundColor = UIColor(red: 0.7, green: 0.2, blue: 0.2, alpha: 0.2)
+        
+        //角丸
+        button.layer.cornerRadius = 10
+        
+        //ボーダー幅
+        button.layer.borderWidth = 1
+        
+        //ボタンをタップした時に実行するメソッドを指定
+        button.addTarget(self, action: "tapped:", forControlEvents:.TouchUpInside)
+        
+        //viewにボタンを追加する
+        self.view.addSubview(button)
+        
+        
+        
+        // UITextFieldを作成する.
+        let myTextField: UITextField = UITextField(frame: CGRectMake(0,0,200,30))
+        
+        // 表示する文字を代入する.
+        myTextField.text = "Hello Swift!!"
+        
+        // Delegateを設定する.
+        myTextField.delegate = self
+        
+        // 枠を表示する.
+        myTextField.borderStyle = UITextBorderStyle.RoundedRect
+        
+        // UITextFieldの表示する位置を設定する.
+        myTextField.layer.position = CGPoint(x:self.view.bounds.width/2,y:300);
+        
+        // Viewに追加する.
+        self.view.addSubview(myTextField)
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func tapped(button: UIButton) {
+        NSLog("test");
+    }
+    
+    /*
+    UITextFieldが編集された直後に呼ばれる.
+    */
+    func textFieldDidBeginEditing(textField: UITextField){
+        println("textFieldDidBeginEditing:" + textField.text)
+    }
+    
+    /*
+    UITextFieldが編集終了する直前に呼ばれる.
+    */
+    func textFieldShouldEndEditing(textField: UITextField) -> Bool {
+        println("textFieldShouldEndEditing:" + textField.text)
+        
+        return true
+    }
+    
+    /*
+    改行ボタンが押された際に呼ばれる.
+    */
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    
+    
+}
+
